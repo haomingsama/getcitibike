aba217ccb06140722b2a6744f354a0064ceb848f
haomingsama <43077597+haomingsama@users.noreply.github.com>
2018-10-30 22:27:12 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='getcitibike',
+      version='1.0',
+      description='help you to download citibike dataset',
+      author='Haoming Yang',
+      author_email='hy1528@nyu.edu',
+      url='https://github.com/haomingsama/getcitibike',
+      py_modules=['getCitiBikeCSV'],
+      install_requires=['os']
+     )
